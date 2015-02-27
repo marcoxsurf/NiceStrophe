1e1dbd58a092929d258606defa1f15188b43b958
Marco Fantasia <fantasia.marco@gmail.com>
2015-02-27 16:59:37 +0100

    improvements

--- NiceStrophe/src/main.h
+++ NiceStrophe/src/main.h
@@ -1,5 +1,5 @@
 /* globals */
-#include <glib.h>
+#include <glib-2.0/glib.h>
 #include "nice.h"
 
 extern const char* const prog_version;
@@ -9,7 +9,3 @@ extern int prog_running;
 GMainLoop *gloop;
 Nice_info *nice_info;
 nice_status_t _nice_status;
-//char *my_jid;
-//char *my_key64;
-//char *other_key64;
-//char *other_jid;
